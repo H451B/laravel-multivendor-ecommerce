adb2fd2dec5539dda9bf6c05c98f330dff03ee62
A. M. Hasibul Hasan <53710515+H451B@users.noreply.github.com>
2023-09-27 09:51:27 +0600

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,19 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Brand;
-use App\Models\Category;
-use App\Models\User;
+
 use App\Models\Product;
-use Carbon\Carbon;
+use App\Models\Category;
+use App\Models\Brand;
+use App\Models\User;
 use Illuminate\Http\Request;
-use Image;
+use Illuminate\Support\Carbon;
+
 class ProductController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        $products = Product::latest()->get();
+        //
+        $products = Product::all();
         return view('admin.products.index',compact('products'));
     }
 
@@ -22,10 +26,10 @@ class ProductController extends Controller
      */
     public function create()
     {
-        $activeVendor = User::where('status','active')->where('role','vendor')->latest()->get();
+        $vendors = User::where('status','active')->where('role','vendor')->latest()->get();
         $brands = Brand::latest()->get();
         $categories = Category::latest()->get();
-        return view('admin.products.create',compact('brands','categories','activeVendor'));
+        return view('admin.products.create',compact('brands','categories','vendors'));
     }
 
     /**
@@ -33,16 +37,16 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $image =$request->file('product_image');
-        $fileName = hexdec(uniqid()).'.'.
-            $image->getClientOriginalExtension();
-        Image::make($image)->resize(1920,622)->save('upload/product/'.$fileName);
-        $save_url = 'upload/product/'.$fileName;
+        //\
+        $image = $request->file('product_image');
+        $filename = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        'Image'::make($image)->resize(1076,507)->save('upload/product/'.$filename);
+        $save_url = 'upload/product/'.$filename;
         Product::create([
             'brand_id'=>$request->brand_id,
             'category_id'=>$request->category_id,
             'product_name'=>$request->product_name,
-            'product_slug'=> strtolower(str_replace('','_', $request->product_name)),
+            'product_slug'=>strtolower(str_replace('','-',$request->product_name)),
             'product_code'=>$request->product_code,
             'product_qty'=>$request->product_qty,
             'product_tags'=>$request->product_tags,
@@ -53,16 +57,16 @@ class ProductController extends Controller
             'short_descp'=>$request->short_descp,
             'long_descp'=>$request->long_descp,
             'hot_deals'=>$request->hot_deals,
-            'featured'=>$request->featured,
-            'special_offer'=>$request->special_offer,
             'special_deals'=>$request->special_deals,
-             'vendor_id'=>$request->vendor_id,
-            'status'=>1,
+            'special_offer'=>$request->special_offer,
+            'featured'=>$request->featured,
+            'vendor_id'=>$request->vendor_id,
             'product_image'=>$save_url,
+            'status'=>1,
             'created_at'=>Carbon::now(),
-        ]);
 
-        return redirect()->route('products.index');
+        ]);
+        // return redirect()->route('products.index');
     }
 
     /**
@@ -80,9 +84,10 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        return view('admin.products.edit',[
-            'product'=>$product
-        ]);
+        $vendors = User::where('status','active')->where('role','vendor')->latest()->get();
+        $brands = Brand::latest()->get();
+        $categories = Category::latest()->get();
+        return view('admin.products.edit',['product'=>$product],compact('brands','categories','vendors'));
     }
 
     /**
@@ -90,7 +95,47 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $oldImage = $product->product_image;
+
+        if ($request->hasFile('product_image')) {
+            $image = $request->file('product_image');
+            $filename = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            $image->move('upload/product/', $filename);
+            $save_url = 'upload/product/' . $filename;
+
+            /*
+             * if updated delete old image from upload/Product folder
+             */
+            // if (file_exists($oldImage)) {
+            //     unlink($oldImage);
+            // }
+        } else {
+            $save_url = $product->product_image;
+        }
+
+        $product->update([
+            'brand_id'=>$request->brand_id,
+            'category_id'=>$request->category_id,
+            'product_name'=>$request->product_name,
+            'product_slug'=>strtolower(str_replace('','-',$request->product_name)),
+            'product_code'=>$request->product_code,
+            'product_qty'=>$request->product_qty,
+            'product_tags'=>$request->product_tags,
+            'product_size'=>$request->product_size,
+            'product_color'=>$request->product_color,
+            'selling_price'=>$request->selling_price,
+            'discount_price'=>$request->discount_price,
+            'short_descp'=>$request->short_descp,
+            'long_descp'=>$request->long_descp,
+            'hot_deals'=>$request->hot_deals,
+            'special_deals'=>$request->special_deals,
+            'special_offer'=>$request->special_offer,
+            'featured'=>$request->featured,
+            'vendor_id'=>$request->vendor_id,
+            'status'=>1,
+            'product_image'=>$save_url,
+        ]);
+        return redirect()->route('products.index');
     }
 
     /**
@@ -98,6 +143,17 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return redirect()->route('products.index');
+    }
+
+    public function ProductInactive($id){
+        Product::findOrFail($id)->update(['status'=>0]);
+        return redirect()->route('products.index');
+    }
+
+    public function ProductActive($id){
+        Product::findOrFail($id)->update(['status'=>1]);
+        return redirect()->route('products.index');
     }
 }
